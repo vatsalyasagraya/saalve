3be4f98aca3b3c7cf1a608617f96c1c3caa94c4b
vatsalyasagraya <2020uec1198@mnit.ac.in>
2023-03-22 19:50:13 +0530

    more components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
-import NavBar from "./components/NavBar";
+import NavBar from "./components/navbar/NavBar";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Meal from "./components/meal/Meal";
+import Bakery from "./components/bakery/Bakery";
+import Grocery from "./components/grocery/Grocery";
+import Partner from "./components/partner/Partner";
+import Favorite from "./components/favorite/Favorite";
+import Cart from "./components/cart/Cart";
+import Account from "./components/account/Account";
+import FoodComp from "./components/FoodComp";
 
 function App() {
   return (
@@ -14,6 +27,7 @@ function App() {
             <Route exact path="/cart" element={<Cart/>}/>
             <Route exact path="/account" element={<Account/>}/>
         </Routes>
+        <FoodComp/>
       </Router>
     </>
   );
