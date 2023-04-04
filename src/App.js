3b1870f668c5206a250bed9dfcac234a17f2df00
Dishant <rathidishant39@gmail.com>
2023-04-04 22:21:40 +0530

    4/4 tkk ka pura mera code

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ import FoodComp from "./components/foodcomp/FoodComp";
 import Carousel from "./components/carousel/Carousel";
 import LoggIn from "./components/login/loggin";
 import Home from "./components/home/Home";
+import Profile from "./components/profile/Profile"
+import Footer from "./components/footer/Footer";
+import NewsLetter from "./components/newsletter/NewsLetter";
 
 
 function App() {
@@ -23,22 +26,33 @@ function App() {
     <>
     <Router>
       <NavBar/>
-        <Routes>
-            <Route exact path="/meal" element={<Meal/>}/>
-        </Routes>
-        {/* <Carousel/>         */}
-        <Routes>
+        {/* <Routes>
+            <Route exact path="/" element={<Home/>}/>
+        </Routes> */}
+        {/* <Carousel/>  */}
+   
+
+        {/* <Routes>
             <Route exact path="/grocery" element={<Grocery/>}/>
+            <Route exact path="/meal" element={<Meal/>}/>
             <Route exact path="/bakery" element={<Bakery/>}/>
             <Route exact path="/partnerwithus" element={<Partner/>}/>
             <Route exact path="/favorite" element={<Favorite/>}/>
             <Route exact path="/cart" element={<Cart/>}/>
             <Route exact path="/account" element={<Account/>}/>
         </Routes>
-        <FoodComp/>
+        <FoodComp/> */}
         
-      </Router>
-      <LoggIn/>
+      </Router> 
+        
+      
+      {/* <LoggIn/>  */}
+       <Profile/>
+      {/* <FoodComp/>  */}
+
+      <Footer/> 
+
+
     </>
   );
 }
---
+++ src/components/profile/Profile.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import profilec from '../images/profile.png';
+import './profile.css'
+
+function Profile() {
+  return (
+    <div>
+      <div className="main-body">
+        <div className="top">
+          <div className="left-image">
+            <img src={profilec} alt="Not found" />
+            <h5>Dishant Rathi</h5>
+          </div>
+          <div className="right-content">
+            <span className="material-icons">edit</span>
+            <p>Edit Profile</p>
+          </div>
+        </div>
+        <div className="down-body">
+          <div className="side-bodd">
+            <div className="side-bar">
+              <h3>A C T I V I T Y</h3>
+              <ul>
+                <li>Reviews</li>
+                <li>Photoes</li>
+                <li>Order History</li>
+                <li>My Adress</li>
+                <li>Favourite Orders</li>
+              </ul>
+            </div>
+            <div className="side-bar">
+              <h3>P A Y M E N T S</h3>
+              <ul>
+                <li>Lorem ip</li>
+                <li>Lorem ip</li>
+                <li>Lorem ip</li>
+                <li>Lorem ip</li>
+                <li>Lorem ip</li>
+              </ul>
+            </div>
+          </div>
+          <div className="side-info">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore beatae ipsam adipisci dolor tempora facilis omnis libero, corrupti similique, quia eos. Ullam non maiores dolores nisi quam vero asperiores unde?</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
